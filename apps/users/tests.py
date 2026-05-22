a0e1c6710b279e2864dcedcae8c6a53773f7085b
Mashiro2465 <rlaalstjr2465@gmail.com>
2026-05-23 01:19:13 +0900

    fix: 테스트 함수 타입 힌트 추가

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -6,12 +6,12 @@ from apps.users.apps import UsersConfig
 class UsersAppTestCase(TestCase):
     """Users 앱 기본 테스트"""
 
-    def test_app_config(self):
+    def test_app_config(self) -> None:
         """앱 설정이 올바른지 확인"""
         self.assertEqual(UsersConfig.name, "apps.users")
         self.assertEqual(UsersConfig.verbose_name, "사용자")
 
-    def test_app_ready(self):
+    def test_app_ready(self) -> None:
         """앱이 정상적으로 로드되는지 확인"""
         # 앱이 로드되면 이 테스트는 통과
         self.assertTrue(True)
