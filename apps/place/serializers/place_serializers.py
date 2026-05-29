365d10359ddf7eb4e2e15d1a15f07323ac458413
yimiro <s_icay@naver.com>
2026-05-30 03:25:13 +0900

    타입: 제목

--- apps/place/serializers/place_serializers.py
+++ apps/place/serializers/place_serializers.py
@@ -3,19 +3,20 @@ from rest_framework import serializers
 from apps.place.models import Place, Tag
 
 
-class TagSerializer(serializers.ModelSerializer):
+class TagSerializer(serializers.ModelSerializer[Tag]):
     class Meta:
         model = Tag
         fields = ["id", "tag_name"]
 
 
-class PlaceListSerializer(serializers.ModelSerializer):
+class PlaceListSerializer(serializers.ModelSerializer[Place]):
     image_url = serializers.SerializerMethodField()
     bookmark_count = serializers.IntegerField(read_only=True)
     tags = TagSerializer(many=True, read_only=True)
 
-    def get_image_url(self, obj):
-        return obj.images.filter(is_main=True).first().image_url
+    def get_image_url(self, obj: Place) -> str | None:
+        image = obj.images.filter(is_main=True).first()
+        return image.image_url if image else None
 
     class Meta:
         model = Place
@@ -30,12 +31,12 @@ class PlaceListSerializer(serializers.ModelSerializer):
         ]
 
 
-class PlaceListResponseSerializer(serializers.Serializer):
+class PlaceListResponseSerializer(serializers.Serializer):  # type: ignore[type-arg]
     count = serializers.IntegerField()
     next = serializers.CharField(allow_null=True)
     previous = serializers.CharField(allow_null=True)
     results = PlaceListSerializer(many=True)
 
 
-class PlaceErrorResponseSerializer(serializers.Serializer):
+class PlaceErrorResponseSerializer(serializers.Serializer):  # type: ignore[type-arg]
     error_detail = serializers.CharField()
