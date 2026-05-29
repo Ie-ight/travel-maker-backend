4c7f301d26ee4f369619b635aba61c73f078c50d
Mashiro2465 <rlaalstjr2465@gmail.com>
2026-05-29 16:02:41 +0900

    fix: AUTH_USER_MODEL 커스텀 User 설정 추가

--- config/settings/base.py
+++ config/settings/base.py
@@ -47,6 +47,8 @@ LOCAL_APPS = [
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
+AUTH_USER_MODEL = "user.User"
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
