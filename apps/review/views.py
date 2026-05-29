b072a05bf8b491d8bd20ad9d2487d7d069ef05b9
Mashiro2465 <rlaalstjr2465@gmail.com>
2026-05-29 15:54:39 +0900

    fix: mypy 타입 수정

--- apps/review/views.py
+++ apps/review/views.py
@@ -1,9 +1,9 @@
 from rest_framework.views import APIView
 
 
-class PlaceReviewListCreateView(APIView):
+class PlaceReviewListCreateView(APIView):  # type: ignore[misc]
     pass
 
 
-class ReviewDetailView(APIView):
+class ReviewDetailView(APIView):  # type: ignore[misc]
     pass
