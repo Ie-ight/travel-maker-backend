8fd9720d542f7d8b2d09a5d0e5b05096db825f39
yimiro <s_icay@naver.com>
2026-05-29 09:53:59 +0900

    타입: 제목
    feat     : 모델 필요 요소 추가 작성

--- apps/place/models.py
+++ apps/place/models.py
@@ -7,7 +7,8 @@ class Place(TimeStampModel):
     place_name = models.CharField(max_length=50)
     latitude = models.DecimalField(max_digits=10, decimal_places=7)
     longitude = models.DecimalField(max_digits=10, decimal_places=7)
-    rating_avg = models.DecimalField(max_digits=2, decimal_places=1, null=True, blank=True)
+    rating_avg = models.DecimalField(max_digits=2, decimal_places=1, null=True, blank=True)  # 평균 평점
+    rating_count = models.PositiveIntegerField(default=0)  # 평점 개수 - 계산 보정용
     description = models.TextField(null=True, blank=True)
     tags = models.ManyToManyField("Tag", related_name="places", blank=True)
 
@@ -21,8 +22,10 @@ class Place(TimeStampModel):
 class PlaceImage(models.Model):
     place = models.ForeignKey(Place, related_name="images", on_delete=models.CASCADE)
     image_url = models.CharField(max_length=255, default="default_image_url")
+    is_main = models.BooleanField(default=False)  # 메인 사진 구분용
+    order = models.PositiveSmallIntegerField(default=0)  # 사진 순서
 
 
 class Tag(models.Model):
     tag_type = models.CharField(max_length=20)
-    tag_name = models.CharField(max_length=20)
+    tag_name = models.CharField(max_length=20, unique=True)
