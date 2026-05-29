24ff9bc0a43f033338651a5e485ced0e4c84d0a0
yimiro <s_icay@naver.com>
2026-05-30 03:25:13 +0900

    타입: 제목

--- apps/place/services/place_services.py
+++ apps/place/services/place_services.py
@@ -1,8 +1,8 @@
-from django.db.models import Count
+from django.db.models import Count, QuerySet
 
 from apps.place.models import Place
 
 
-def get_place_list():
+def get_place_list() -> QuerySet[Place]:
     queryset = Place.objects.prefetch_related("images", "tags").annotate(bookmark_count=Count("bookmarks"))
     return queryset
