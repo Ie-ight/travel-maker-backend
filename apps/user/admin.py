7985e90f164c8720642e5a30e1b8367891173884
Mashiro2465 <rlaalstjr2465@gmail.com>
2026-06-04 13:55:19 +0900

    feat: Django Admin 리뷰 관리 추가

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -6,6 +6,9 @@ from apps.user.models import User
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):  # type: ignore[type-arg]
     list_display = ["id", "nickname", "email", "is_active", "role", "created_at"]
+    list_display_links = ["id", "nickname"]
     list_filter = ["is_active", "role"]
     search_fields = ["nickname", "email"]
     ordering = ["-created_at"]
+    readonly_fields = ["created_at", "updated_at", "last_login"]  # 읽기 전용
+    exclude = ["password"]  # 비밀번호 숨기기
